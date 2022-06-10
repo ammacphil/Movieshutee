4acd1555e20d15f60f74347c7ec634ff130b02c7
amPhilip <philipmwangi91@gmail.com>
2022-06-10 05:12:48 +0300

    movieshutee:modified some minor changes

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -3,7 +3,9 @@
 </script>
 
 <div class="movie">
-	<img src={'https://image.tmdb.org/t/p/w500' + movie.poster_path} alt={movie.title} />
+	<a href={'/movie/' + movie.id}>
+		<img src={'https://image.tmdb.org/t/p/w500' + movie.poster_path} alt={movie.title} />
+	</a>
 	<div class="descr">
 		<h4>{movie.title}</h4>
 		<span class="details">
@@ -17,25 +19,25 @@
 	img {
 		width: 100%;
 		object-fit: contain;
-        margin-bottom: 1rem;
+		margin-bottom: 1rem;
 	}
 	.movie {
 		cursor: pointer;
-        display: flex;
-        flex-direction: column;
-        padding: 00.75rem;
-        justify-content: space-around;
+		display: flex;
+		flex-direction: column;
+		padding: 00.75rem;
+		justify-content: space-around;
 	}
-    .descr{
-        /*background-color: red;*/
-        height: 10vh;
-    }
-    .details{
-        width: 100%;
-        display: flex;
-        gap: 20px;
-    }
-    p{
-        font-size: 0.75rem;
-    }
+	.descr {
+		/*background-color: red;*/
+		height: 10vh;
+	}
+	.details {
+		width: 100%;
+		display: flex;
+		gap: 20px;
+	}
+	p {
+		font-size: 0.75rem;
+	}
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,12 @@
 <script context="module">
 	export async function load({ fetch }) {
 		//fetch data
-		const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=58791a9d5f40e2836e8f1c41d2f01222&language=en-US&page=1`);
+		const res = await fetch(
+			`https://api.themoviedb.org/3/movie/popular?api_key=58791a9d5f40e2836e8f1c41d2f01222&language=en-US&page=1`
+		);
 		//get the fetch json data
 		const data = await res.json();
-        console.log(data)
+		console.log(data);
 		if (res.ok) {
 			//return the fetched result
 			return {
@@ -15,13 +17,11 @@
 </script>
 
 <script>
-    import PopularMovies from "../components/PopularMovies.svelte";
-    export let popular;
-    //console.log(popular);
+	import PopularMovies from '../components/PopularMovies.svelte';
+	export let popular;
+	//console.log(popular);
 </script>
 
-
 <section>
-    <PopularMovies {popular}/>
-
+	<PopularMovies {popular} />
 </section>
