f166e91439e01b4b01471c467856f121f2dd03fb
amPhilip <philipmwangi91@gmail.com>
2022-06-10 03:22:19 +0300

    movieshutee:Adding useful category components to the app

---
+++ src/components/PopularMovies.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let popular;
+	//console.log(popular)
+	import Card from './Card.svelte';
+</script>
+
+<h3>Popular Movies</h3>
+
+<section class="popular__movies">
+	<!--{#each}{/each} => forEach function-->
+	{#each popular as movie}
+		<Card  {movie}/>
+	{/each}
+</section>
