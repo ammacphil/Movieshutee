4c135af8dd6041dde7797f10c3eeabc610fa26fd
amPhilip <philipmwangi91@gmail.com>
2022-06-10 03:24:20 +0300

    movieshutee:adding the api key

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+	export async function load({ fetch }) {
+		//fetch data
+		const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=58791a9d5f40e2836e8f1c41d2f01222&language=en-US&page=1`);
+		//get the fetch json data
+		const data = await res.json();
+        console.log(data)
+		if (res.ok) {
+			//return the fetched result
+			return {
+				props: { popular: data.results }
+			};
+		}
+	}
+</script>
+
+<script>
+    import PopularMovies from "../components/PopularMovies.svelte";
+    export let popular;
+    //console.log(popular);
+</script>
+
+
+<section>
+    <PopularMovies {popular}/>
+
+</section>
